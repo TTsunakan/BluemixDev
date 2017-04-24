91132366af8fed713318d462629c6281126f65c6
Tsubasa <Tsubasa@LAPTOP-B3G4GVTK>
2017-04-24 22:24:43 +0900

    お試し実行

--- src/main/java/wasdev/sample/model/HelloBean.java
+++ src/main/java/wasdev/sample/model/HelloBean.java
@@ -1,3 +1,49 @@
 package wasdev.sample.model;
 
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+
+public class HelloBean implements Serializable{
+	
+	private static final long serialVersionUID = 2324320609815434023L;
+	private static final String MESSAGE_TBMPLATE = "【%s】こんにちわ Bluemix[%d]";
+	
+	private int count = 0;
+	
+	private String message = String.format(MESSAGE_TBMPLATE,"デフォルト値",count);
+	
+	@PostConstruct
+	public void init(){
+		setCount(0);
+		setMessage(String.format(MESSAGE_TBMPLATE, "@PostConstruct",getCount()));
+	}
+	
+	public void action(){
+		int next = getCount() + 1;
+		setCount(next);
+		setMessage(String.format(MESSAGE_TBMPLATE, "ボタンアクション",next));
+	}
+	
+	
+	
+	public String getMessage(){
+		return message;
+	}
+	
+	public void setMessage(String message){
+		this.message = message;
+	}
+	
+	public int getCount(){
+		return count;
+	}
+	
+	public void setCount(int count){
+		this.count = count;
+	}
+}
---
+++ src/main/java/wasdev/sample/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package wasdev.sample.servlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import wasdev.sample.model.HelloBean;
+import javax.inject.*;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet{
+	private static final long serialVersionUID = 1808808594661653378L;
+	@Inject
+	private HelloBean helloBean;
+	
+	public void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException{
+		res.setContentType("text/html charset=UTF-8");
+		
+		PrintWriter out = res.getWriter();
+		out.println("<html><body>");
+		out.println("<hrad><link rel=\"stylesheet\" href=\"\"/>");
+		out.println("<h1>" + helloBean.getMessage() +"</h1>");
+		out.println("</body></html>");
+		
+	}
+}
